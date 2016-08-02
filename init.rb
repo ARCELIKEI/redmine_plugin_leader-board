81d756ac0643cb2a046c4c6cf5e4caf93655aa42
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2016-08-02 13:30:02 +0300

    url, description added.

--- init.rb
+++ init.rb
@@ -1,15 +1,14 @@
 Redmine::Plugin.register :leader_board do
   name 'Leader Board plugin'
   author 'Arcelik AR-GE'
-  description 'This is a plugin for Redmine'
+  description 'This plugin provides adding team hierarchy. Team leaders can see their members activities from Managerial section.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ARCELIKEI/redmine_plugin_leader-board'
 
    menu :admin_menu, :team_managers, {:controller => 'team_managers', :action => 'index'},
   		:caption => :team_managers_admin
 
-   menu :account_menu, :my_link, {:controller => 'team_details', :action => 'index'},   ### make init HERE!!!
+   menu :account_menu, :my_link, {:controller => 'team_details', :action => 'index'},
        :caption => :managerial, :if => Proc.new { (TeamManager.find_by_user_id(User.current.id)) && (User.current.logged?) }
 
 end
